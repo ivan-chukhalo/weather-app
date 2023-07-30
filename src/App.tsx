b7a7be25b473523e4c2267710f1f09e8356f1bd2
Ivan_Chukhalo <ivanchukhalo@gmail.com>
2023-07-30 13:09:26 +0300

    style: add tailwind styles for initial page block

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      The Weather App
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
+        Weather App
+      </section>
     </main>
   )
 }
