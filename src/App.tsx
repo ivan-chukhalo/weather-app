c34c943e4ebd76a8beba9f1afb49fde082ab412d
Ivan_Chukhalo <ivanchukhalo@gmail.com>
2023-07-30 13:25:13 +0300

    feat: accomplish creating initial page block

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,23 @@ const App = () => {
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
-        Weather App
+        <h1 className="text-4xl font-thin">
+          Weath<span className="font-black">Up</span>
+        </h1>
+        <p className="text-sm mt-2">
+          Enter below a place you want to know the weather of and select an
+          option from downdrop
+        </p>
+        <div className="flex mt-10 md:mt-4">
+          <input
+            type="text"
+            value={'type in'}
+            className="px-2 py-1 rounded-l-md border-2 border-white"
+          />
+          <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer">
+            search
+          </button>
+        </div>
       </section>
     </main>
   )
