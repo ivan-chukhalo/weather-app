beb94baa5fd21b4b15a3ab19b3b0d5c981540068
Ivan_Chukhalo <ivanchukhalo@gmail.com>
2023-07-31 15:46:26 +0300

    feat: add dropdown list of options

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import { ChangeEvent, useState } from 'react'
+import  {optionType} from './types'
 
 const App = (): JSX.Element => {
   const [term, setTerm] = useState<string>('')
+  const [options, setOptions] = useState<[]>([])
 
   const getSearchOptions = (value: string) => {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=${5}&appid=${process.env.REACT_APP_API_KEY}`)
-    .then((res) => res.json())
-    .then((data) => console.log({data}))
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=${5}&appid=${
+        process.env.REACT_APP_API_KEY
+      }`
+    )
+      .then((res) => res.json())
+      .then((data) => setOptions(data))
   }
-  
+
   const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value.trim()
     setTerm(value)
     if (value === '') return
     getSearchOptions(value)
+  }
+
+  const onOptionSelect = (option: optionType) => {
+    console.log(option.name)
   }
 
   return (
@@ -26,13 +36,22 @@ const App = (): JSX.Element => {
           Enter below a place you want to know the weather of and select an
           option from downdrop
         </p>
-        <div className="flex mt-10 md:mt-4">
+        <div className="relative flex mt-10 md:mt-4">
           <input
             type="text"
             value={term}
             className="px-2 py-1 rounded-l-md border-2 border-white"
             onChange={onInputChange}
           />
+          <ul className="absolute top-9 bg-white m1-1 rounded-b-md">
+            {options.map((option: optionType, index: number) => (
+              <li key={option.name + '_' + index}>
+                <button className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer" onClick={()=>onOptionSelect(option)}>
+                  {option.name}
+                </button>
+              </li>
+            ))}
+          </ul>
           <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer">
             search
           </button>
