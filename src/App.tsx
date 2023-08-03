766429179683859c6034b74f366a5e5dc1b8bfdb
Ivan_Chukhalo <ivanchukhalo@gmail.com>
2023-08-03 14:47:54 +0300

    feat: forecast now shows the geo, country, clouds, temperature and temperature min-max for today

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, useEffect, useState } from 'react'
 import { optionType } from './types'
+import Forecast from './components/Forecast'
 import Search from './components/Search'
 import useForecast from './hooks/useForecast'
 
@@ -15,7 +16,7 @@ const App = (): JSX.Element => {
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
       {forecast ? (
-        <p>FORECAST</p>
+        <Forecast data={forecast} />
       ) : (
         <Search
           term={term}
